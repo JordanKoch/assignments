b7af7e1bf1d0e4a841ce23701c445fb172e1163a
Jordan Koch <jordantylerkoch@gmail.com>
2014-04-15 20:52:50 -0400

    deleted all code

--- homework-10.1-tests.js
+++ homework-10.1-tests.js
@@ -1,18 +1,3 @@
-/*
-//Rewrote code up to function
-//Successful TDD up to that point
-//Getting the hang of writing js
-*/
-
 var assert = require("assert"),
     hallway = require("./2014-04-04-code.js");
-
-assert(hallway);
-assert(hallway.doors);
-
-assert.equal(hallway.CLOSED, -1);
-assert.equal(hallway.OPENED, +1);
-
-assert.equal(hallway.toggle(hallway.CLOSED), hallway.OPENED);
-assert.equal(hallway.toggle(hallway.OPENED), hallway.CLOSED);
-assert.equal(hallway.toggle('askew'), undefined);
+]
