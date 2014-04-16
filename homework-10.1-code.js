36c1c35fdf75ae5df94935e50d2ba79a886e4f68
Jordan Koch <jordantylerkoch@gmail.com>
2014-04-15 21:49:36 -0400

    goal:get all doors open on first pass

---
+++ homework-10.1-code.js
@@ -0,0 +1,20 @@
+module.exports = {
+    CLOSED: 0,
+    OPENED: 1,
+
+    doors : [0, 0, 0, 0, 0],
+    
+    toggle: function(door){
+        if (door === this.CLOSED) return this.OPENED;
+        
+        if (door === this.OPENED) return this.CLOSED;
+    },
+    
+    walk : function(pass) {
+        for (var door = pass; door < this.doors.length; door++) {
+            
+        
+        return this.doors;
+        }
+    },
+};
